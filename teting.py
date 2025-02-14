ef1b7180c5faf515b29d833efa3bf8331ba5031d
SSAKTHIVELMURUGAN <s.sakthivelmurugan2003@gmail.com>
2025-02-14 17:08:27 +0530

    dev

---
+++ teting.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import subprocess
+def get_files(file_path):
+    print(file_path)
+    check = subprocess.check_output(["python","--version"], text=True)
+    check_path = os.path.exists(file_path)
+    print(os.path.abspath("myenv"))  
+
+    print(check_path)
+    print(check)
+
+if __name__ == "__main__":
+    get_files("src/main.py")
+    get_files("my_code_validator/cli.py")
+    get_files("my_code_validator/commands/validate_project.py")
